9633db3bc66489ce389555114c6a5d326eb3df73
Vijay Shelke <vdshelke126@gmail.com>
2021-03-14 21:19:58 +0530

    first commit

--- src/app/Employee/employeeModel.ts
+++ src/app/Employee/employeeModel.ts
@@ -1,29 +1,14 @@
 export interface EmployeeModel{
     id?:number,
     name:string,
-    username:string,
-    email:string,
     address:EmployeeAddress,
     phone:string
-    website:string
-    company:EmployeeCompany
+
 
 }
 export interface EmployeeAddress{
-    street?:string,
-    suite?:string,
     city?:string,
-    zipcode?:string,
-    geo?:EmployeeAddressGeo
-}
-
-export interface EmployeeAddressGeo{
-    lat:number,
-    lng:number
-}
-
-export interface EmployeeCompany{
-    name:string,
-    catchPhrase?:string,
-    bs?:string
+    address1:string,
+    address2:string,
+    postalCode:string
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { HeaderComponent } from './header/header.component';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PortalModule } from '@angular/cdk/portal';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { EmployeeRegisterComponent } from './Employee/employee-register/employee-register.component';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     EmployeeListComponent,
-        LoginComponent,
-    RegisterComponent,
     EmployeeRegisterComponent
   ],
   imports: [
@@ -32,7 +28,9 @@ import { EmployeeRegisterComponent } from './Employee/employee-register/employee
     MaterialModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    PortalModule
+    PortalModule,
+    MatSortModule,
+    MatTableModule
   ],
   entryComponents: [],
   providers: [],
